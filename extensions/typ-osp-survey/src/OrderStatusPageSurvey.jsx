c99797159d07b6edcb9ffe05c5d74315433251f4
Brianna Leung <brianna.leung@shopify.com>
2025-09-17 15:16:45 -0400

    add survey tutorial  app and extensions

---
+++ extensions/typ-osp-survey/src/OrderStatusPageSurvey.jsx
@@ -0,0 +1,56 @@
+import { render } from "preact";
+import { useState } from "preact/hooks";
+import { Survey, useStorageState } from "./shared.jsx";
+
+export default function () {
+  render(<ProductReview />, document.body);
+}
+
+function ProductReview() {
+  const [productReview, setProductReview] = useState("");
+  const [loading, setLoading] = useState(false);
+  // Store into local storage if the product was reviewed by the customer.
+  const [productReviewed, setProductReviewed] =
+    useStorageState("product-reviewed");
+
+  async function handleSubmit() {
+    // Simulate a server request
+    setLoading(true);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        // Send the review to the server
+        console.log("Submitted:", productReview);
+        setLoading(false);
+        setProductReviewed(true);
+        resolve();
+      }, 750);
+    });
+  }
+
+  // Hides the survey if the product has already been reviewed
+  if (productReviewed.loading || productReviewed.data) {
+    return null;
+  }
+
+  return (
+    <Survey
+      title="How do you like your purchase?"
+      description="We would like to learn if you are enjoying your purchase."
+      onSubmit={handleSubmit}
+      loading={loading}
+    >
+      <s-choice-list
+        name="product-review"
+        value={productReview}
+        onChange={(event) => {
+          setProductReview(event.currentTarget.values[0]);
+        }}
+      >
+        <s-choice value="5">Amazing! Very happy with it.</s-choice>
+        <s-choice value="4">It's okay, I expected more.</s-choice>
+        <s-choice value="3">Eh. There are better options out there.</s-choice>
+        <s-choice value="2">I regret the purchase.</s-choice>
+      </s-choice-list>
+    </Survey>
+  );
+}
---
+++ extensions/typ-osp-survey/src/ThankyouPageSurvey.jsx
@@ -0,0 +1,55 @@
+import { render } from "preact";
+import { useState } from "preact/hooks";
+import { Survey, useStorageState } from "./shared.jsx";
+
+export default function () {
+  render(<Attribution />, document.body);
+}
+
+function Attribution() {
+  const [attribution, setAttribution] = useState("");
+  const [loading, setLoading] = useState(false);
+  // Store into local storage if the attribution survey was completed by the customer.
+  const [attributionSubmitted, setAttributionSubmitted] = useStorageState(
+    "attribution-submitted",
+  );
+
+  async function handleSubmit() {
+    // Simulate a server request
+    setLoading(true);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        // Send the review to the server
+        console.log("Submitted:", attribution);
+        setLoading(false);
+        setAttributionSubmitted(true);
+        resolve();
+      }, 750);
+    });
+  }
+
+  // Hides the survey if the attribution has already been submitted
+  if (attributionSubmitted.loading || attributionSubmitted.data === true) {
+    return null;
+  }
+
+  return (
+    <Survey
+      title="How did you hear about us?"
+      description="We would like to learn if you are enjoying your purchase."
+      onSubmit={handleSubmit}
+      loading={loading}
+    >
+      <s-choice-list
+        name="sale-attribution"
+        value={attribution}
+        onChange={(event) => setAttribution(event.currentTarget.values[0])}
+      >
+        <s-choice value="tv">TV</s-choice>
+        <s-choice value="podcast">Podcast</s-choice>
+        <s-choice value="family">From a friend or family member</s-choice>
+        <s-choice value="tiktok">Tiktok</s-choice>
+      </s-choice-list>
+    </Survey>
+  );
+}
